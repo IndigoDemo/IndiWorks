c0021a3e9d03dfc4c1bdd2b27a28b6fef765a1d3
JoMs <jholen@nepgroup.com>
2018-05-01 16:12:26 +0200

    Simplify track naming in rocket example file

--- src/core/tracks.js
+++ src/core/tracks.js
@@ -2,6 +2,12 @@ class Tracks {
     constructor(syncClient) {
         this.cRot = syncClient.getTrack("c_rotation");
         this.cDist = syncClient.getTrack("c_distance");
+
+        this.clearColor = {
+            r: syncClient.getTrack("c_r"),
+            g: syncClient.getTrack("c_g"),
+            b: syncClient.getTrack("c_g"),
+        };
     }
 }
 
