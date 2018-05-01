bcf62b76c04d7f58997bab865bf5289afde823b1
JoMs <jholen@nepgroup.com>
2018-05-01 16:12:51 +0200

    Pretty pretty

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import Controller from "./core/controller";
 const container = document.getElementById("appContainer");
 container.querySelector("#loading").style.display = "none";
 
-const demoMode = process.env["NODE_ENV"] === "production";
+const demoMode = process.env.NODE_ENV === "production";
 
 const sceneController = new Controller(demoMode);
